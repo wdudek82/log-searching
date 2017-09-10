77a1c6a841b6f11ce42c86b8a17cb2b994dd3b73
Wojciech Dudek <wdudek82@gmail.com>
2017-09-10 10:42:55 +0200

    Added summary to find_value.sh

--- bash_scripts/find_value.sh
+++ bash_scripts/find_value.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 
+# Default value for $1
 greaterThan=${1:-20}
 
 echo "Find in files lines containing vlaues greater than $greaterThan"
+
 find -name "*-0[4-5]-0[7-8].txt" -exec grep -i line {} + | 
 	cut -d " " -f 4 | 
 	cut -d ";" -f 1 | 
-	awk -v var="$greaterThan" '$1>var'
+	awk -v var="$greaterThan" '$1>var' | 
+	tee /dev/tty |  # Redirect output to TTY instead of to file 
+	echo "Found: $(wc -l)"  # Print summary
